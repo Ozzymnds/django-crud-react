0b41fa903e131248154e108ef7414bff42f9df12
Ozzymnds <sanz00zgz@gmail.com>
2024-05-21 11:53:28 +0200

    2o commit del frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { TasksPage } from './pages/TasksPage'
+import { TaskFormPage } from './pages/TaskFormPage'
+import { Navigation } from './components/Navigation'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Navigate to="/tasks" />} />
+        <Route path="/tasks" element={<TasksPage />} />
+        <Route path="/task-create" element={<TaskFormPage />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
